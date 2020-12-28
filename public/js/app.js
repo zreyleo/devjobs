d0e8518f00270f158941b53b2c8ba2353cf5774d
Regynald Zambrano <zreyleo@gmail.com>
2020-12-27 20:29:30 -0500

    Se puede guardar un set de habilidades requeridas para guardar en la base de datos.

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const skills = document.querySelector('.lista-conocimientos');
+
+
+
+    if (skills) {
+        skills.addEventListener('click', agregarSkills);
+    }
+});
+
+const skills = new Set();
+function agregarSkills(event) {
+    if (event.target.tagName == 'LI') {
+        if (event.target.classList.contains('activo')) {
+            skills.delete(event.target.textContent);
+            event.target.classList.remove('activo')
+        } else {
+            skills.add(event.target.textContent);
+            event.target.classList.add('activo')
+        }
+    }
+    console.log(skills)
+    const skillsArray = [...skills];
+    document.querySelector('#skills').value = skillsArray;
+}
