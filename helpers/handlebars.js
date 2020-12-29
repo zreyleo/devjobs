61c3125c9979c0ed4ff51c3dd24ad818d5d7ee20
Regynald Zambrano <zreyleo@gmail.com>
2020-12-29 09:51:23 -0500

    Se recupera las skills seleccionadas y el tipo de contrato de la vacante con ayuda de los helpers. - Actualizado el archivo helpers.js

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,15 +1,21 @@
 module.exports = {
     seleccionarSkills: (seleccionadas = [], opciones) => {
         const skills = ['HTML5', 'CSS3', 'CSSGrid', 'Flexbox', 'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS', 'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP', 'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize', 'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress'];
-        
+
         let html = ``;
-        
-        skills.forEach(s => {
+
+        skills.forEach(skill => {
             html += `
-                <li>${s}</li>
+                <li ${seleccionadas.includes(skill) ? 'class="activo"' : ''}>${skill}</li>
             `
         });
 
         return opciones.fn().html = html;
+    },
+
+    tipoContrato: (seleccionado, opciones) => {
+        return opciones.fn().replace(
+            new RegExp(` value="${seleccionado}"`), '$& selected="selected"'
+        )
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (skills) {
         skills.addEventListener('click', agregarSkills);
+
+        // una vez que estamos en editar, llamar funcion
+        skillsSeleccionados();
     }
 });
 
 const skills = new Set();
+
 function agregarSkills(event) {
     if (event.target.tagName == 'LI') {
         if (event.target.classList.contains('activo')) {
@@ -19,6 +23,17 @@ function agregarSkills(event) {
             event.target.classList.add('activo')
         }
     }
+    const skillsArray = [...skills];
+    document.querySelector('#skills').value = skillsArray;
+}
+
+function skillsSeleccionados() {
+    const seleccionadas = Array.from(document.querySelectorAll('.lista-conocimientos .activo'));
+
+    seleccionadas.forEach(seleccion => {
+        skills.add(seleccion.textContent);
+    })
+
     const skillsArray = [...skills];
     document.querySelector('#skills').value = skillsArray;
 }
