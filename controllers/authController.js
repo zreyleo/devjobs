d716a1fad7320fabeaf726ee58c54352de2d3529
Regynald Zambrano <zreyleo@gmail.com>
2021-01-05 23:21:16 -0500

    Muestra las vacantes propias del autor en el panel de administracion.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const Vacante = require('../models/Vacante');
 
 exports.autenticarUsuario = passport.authenticate('local', {
     successRedirect: '/administracion',
@@ -16,9 +17,12 @@ exports.verificarUsuario = (req, res, next) => {
     return res.redirect('/iniciar-sesion');
 }
 
-exports.mostrarPanel = (req, res) => {
+exports.mostrarPanel = async (req, res) => {
+    const vacantes = await Vacante.find({ autor: req.user._id }).lean();
+
     res.render('administracion', {
         titlePage: 'Panel de Administración',
-        tagline: 'Crea y Administra tus vacantes desde aquí'
+        tagline: 'Crea y Administra tus vacantes desde aquí',
+        vacantes
     })
 }
